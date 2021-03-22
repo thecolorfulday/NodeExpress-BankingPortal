9fc37a0597fcf35fd5407b35cea83df42533466b
William Wang <william.wang03@sap.com>
2021-03-22 18:01:49 +0800

    Use Express to Create a Server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.set('/', path.join(__dirname, 'public'));
+
+app.get('/', (req, res) => {
+  res.render(path.join('views', 'index'), {
+    title: 'Index'
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log('PS Project Running on port 3000!');
+});
