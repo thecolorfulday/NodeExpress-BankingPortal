853714d0d3e5a9f54b610ec59e969abd3ce65931
William Wang <william.wang03@sap.com>
2021-03-22 20:32:28 +0800

    use expresss to create a server

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render(path.join('views', 'index'), {
+  res.render('index', {
     title: 'Index'
   });
 });
